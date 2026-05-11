c3cce6ad82511275f6e0f86fb46ee7e5c86e8d04
Massil <160241880+Massil-br@users.noreply.github.com>
2026-05-11 15:36:45 +0200

    corrected modules import

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from backend.api.routers.user_router import router as user_router
+from routers.user_router import router as user_router
 app = FastAPI()
 
 app.include_router(user_router)
