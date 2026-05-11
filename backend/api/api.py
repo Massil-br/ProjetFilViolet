8e7a866eb6232ebfefbd604bf10820951f94673e
Massil <160241880+Massil-br@users.noreply.github.com>
2026-05-11 15:33:20 +0200

    push failed server launch after importing router

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
-
+from backend.api.routers.user_router import router as user_router
 app = FastAPI()
+
+app.include_router(user_router)
+
+
+
 @app.get("/")
 async def root():
     return {"message":"Welcome to pokerApi"}
